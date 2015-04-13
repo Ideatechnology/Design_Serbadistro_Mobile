93db654d88a7c3a3673756f26e7aa7198d37a14d
Harry Ridwan Ramadan <harryridwanramadan@gmail.com>
2015-04-13 11:03:15 +0700

    Versi1

---
+++ index.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Web Design UI KIT</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+        <link rel="apple-touch-icon-precomposed" href="images/apple-touch-icon.png">
+         <link id="data-uikit-theme" rel="stylesheet" href="css/uikit.min.css">
+        <link id="data-uikit-theme" rel="stylesheet" href="css/uikit.docs.min.css">
+        <link rel="stylesheet" href="css/docs.css">
+        <script src="js/jquery.js"></script>
+        <script src="js/uikit.min.js"></script>
+        <script src="js/docs.js"></script>
+        <script src="js/components/slideshow.js"></script>
+        <script src="js/components/slideshow-fx.js"></script>
+         <script src="js/components/sticky.js"></script>
+         
+    </head>
+    <body>
+  
+ <div data-uk-sticky>
+<div class="uk-margin" style="margin-bottom:0px;">
+<nav class="uk-navbar">
+
+<ul class="uk-navbar-nav">
+    <li ><a href="#offcanvas" class="uk-navbar-toggle " style="font-weight:bold" data-uk-offcanvas></a></li>
+</ul>
+
+<div class="uk-navbar-flip">
+<ul class="uk-navbar-nav">
+    <li>
+    <a href="keranjang.html" style="position:relative">
+        <img src="images/cart.png"> <span style="color:#fff;position:absolute;top:0;right:23px;">0</span> 
+    </a>
+    </li>
+</ul>
+
+</div>
+
+<div class="uk-navbar-content uk-navbar-center" style="font-weight:bold">SERBADISTRO</div>
+
+</nav>
+
+</div>
+
+<div class="uk-grid uk-grid-collapse" style="background:#42505d;">
+<div class="uk-width-7-10" style="background:#42505d">
+ <ul class="uk-subnav uk-subnav-line  subnav-ideo" style="margin-left:0px;">
+    <li><a href="#">News</a></li>
+    <li><a href="#">Tshirt</a></li>
+    <li><a href="#">Jeans</a></li>
+    <li ><a href="#">Jackt</a></li>                  
+</ul>
+
+</div>
+                                    <div class="uk-width-3-10">
+                                        
+<ul class="uk-subnav  subnav-ideo" style="float:right;margin-right:10px">
+ <li data-uk-dropdown="{mode:'click'}">
+                                    <a href="#">Akun <i class="uk-icon-caret-down"></i></a>
+                                    <div class="uk-dropdown uk-dropdown-small">
+                                        <ul class="uk-nav uk-nav-dropdown sub-drop-ideo">
+                                            <li><a href="#">Login</a></li>
+                                            <li><a href="#">Register</a></li>
+                                            <li><a href="#">Affaliate</a></li>
+                                        </ul>
+
+                                    </div>
+                                </li>
+                                <li>
+                                 <a href="javascript:void(0)" data-uk-offcanvas="{target:'#searchcanvas'}"><i class="uk-icon-search"></i></a>
+
+                                </li>
+</ul>
+<div style="clear:both"></div>
+
+                                    </div>
+                                    
+                                </div>
+
+</div>
+
+ 
+
+<div class="uk-slidenav-position" data-uk-slideshow="{animation: 'scale'}">
+                                        <ul class="uk-slideshow">
+                                            <li><img src="images/placeholder_800x400_1.jpg" width="800" height="400" alt=""></li>
+                                            <li><img src="images/placeholder_800x400_2.jpg" width="800" height="400" alt=""></li>
+                                            <li><img src="images/placeholder_800x400_3.jpg" width="800" height="400" alt=""></li>
+                                        </ul>
+                                        <a href="#" class="uk-slidenav uk-slidenav-contrast uk-slidenav-previous" data-uk-slideshow-item="previous" style="color: rgba(255,255,255,0.4)"></a>
+                                        <a href="#" class="uk-slidenav uk-slidenav-contrast uk-slidenav-next" data-uk-slideshow-item="next" style="color: rgba(255,255,255,0.4)"></a>
+                                    </div>
+                               
+
+<div class="uk-grid">
+    <div class="uk-width-1-1"><h1 style="text-align:center" class="uk-margin-bottom uk-margin-top">New Arrivals</h1></div>
+
+  <div class="uk-width-1-1">
+  <div class="uk-margin uk-container-center uk-text-center" data-uk-margin="">
+                               
+                                <a class="uk-thumbnail uk-thumbnail-mini" href="#">
+                                    <img src="images/placeholder_200x100.svg" width="200" height="100" alt="">
+                                    <div class="uk-thumbnail-caption">Caption <code>&lt;a&gt;</code></div>
+                                </a>
+
+                                 <a class="uk-thumbnail uk-thumbnail-mini" href="#">
+                                    <img src="images/placeholder_200x100.svg" width="200" height="100" alt="">
+                                    <div class="uk-thumbnail-caption">Caption <code>&lt;a&gt;</code></div>
+                                </a>
+                                 <a class="uk-thumbnail uk-thumbnail-mini" href="#">
+                                    <img src="images/placeholder_200x100.svg" width="200" height="100" alt="">
+                                    <div class="uk-thumbnail-caption">Caption <code>&lt;a&gt;</code></div>
+                                </a>
+                                   <a class="uk-thumbnail uk-thumbnail-mini" href="#">
+                                    <img src="images/placeholder_200x100.svg" width="200" height="100" alt="">
+                                    <div class="uk-thumbnail-caption">Caption <code>&lt;a&gt;</code></div>
+                                </a>
+                                 </a>
+                                   <a class="uk-thumbnail uk-thumbnail-mini" href="#">
+                                    <img src="images/placeholder_200x100.svg" width="200" height="100" alt="">
+                                    <div class="uk-thumbnail-caption">Caption <code>&lt;a&gt;</code></div>
+                                </a>
+                                 </a>
+                                   <a class="uk-thumbnail uk-thumbnail-mini" href="#">
+                                    <img src="images/placeholder_200x100.svg" width="200" height="100" alt="">
+                                    <div class="uk-thumbnail-caption">Caption <code>&lt;a&gt;</code></div>
+                                </a>
+                            </div>
+
+                            </div>
+</div>
+
+<div class="uk-grid">
+    <div class="uk-width-1-1"><h1 style="text-align:center" class="uk-margin-bottom uk-margin-top">PRODUK</h1></div>
+
+  <div class="uk-width-1-1 uk-container-center uk-text-center" style="background:#fff;border:0">
+  <div class="uk-margin " data-uk-margin="">
+                               
+                                <a class="uk-thumbnail uk-thumbnail-mini" href="#">
+                                    <img src="images/placeholder_200x100.svg" width="200" height="100" alt="">
+                                    <div class="uk-thumbnail-caption">Caption <code>&lt;a&gt;</code></div>
+                                </a>
+
+                                 <a class="uk-thumbnail uk-thumbnail-mini" href="#">
+                                    <img src="images/placeholder_200x100.svg" width="200" height="100" alt="">
+                                    <div class="uk-thumbnail-caption">Caption <code>&lt;a&gt;</code></div>
+                                </a>
+                                 <a class="uk-thumbnail uk-thumbnail-mini" href="#">
+                                    <img src="images/placeholder_200x100.svg" width="200" height="100" alt="">
+                                    <div class="uk-thumbnail-caption">Caption <code>&lt;a&gt;</code></div>
+                                </a>
+                                   <a class="uk-thumbnail uk-thumbnail-mini" href="#">
+                                    <img src="images/placeholder_200x100.svg" width="200" height="100" alt="">
+                                    <div class="uk-thumbnail-caption">Caption <code>&lt;a&gt;</code></div>
+                                </a>
+                                 </a>
+                                   <a class="uk-thumbnail uk-thumbnail-mini" href="#">
+                                    <img src="images/placeholder_200x100.svg" width="200" height="100" alt="">
+                                    <div class="uk-thumbnail-caption">Caption <code>&lt;a&gt;</code></div>
+                                </a>
+                                 </a>
+                                   <a class="uk-thumbnail uk-thumbnail-mini" href="#">
+                                    <img src="images/placeholder_200x100.svg" width="200" height="100" alt="">
+                                    <div class="uk-thumbnail-caption">Caption <code>&lt;a&gt;</code></div>
+                                </a>
+                                 </a>
+                                  
+                            </div>
+
+                            </div>
+</div>
+
+
+
+
+
+
+<div class="tm-footer uk-margin-top" style="background:#42505d;">
+            <div class="uk-container uk-container-center uk-text-center">
+
+                <ul class="uk-subnav uk-subnav-line uk-flex-center">
+                    <li><a href="https://github.com/uikit/uikit">How To Order </a></li>
+                    <li><a href="https://github.com/uikit/uikit/issues">Return Excange</a></li>
+                    <li><a href="https://github.com/uikit/uikit/blob/master/CHANGELOG.md">Privacy And Security</a></li>
+                    <li><a href="https://twitter.com/getuikit">Shipping And Delivery</a></li>
+                </ul>
+
+                <div class="uk-margin-bottom sosmed-ico" style="color:#000">
+                <span href="#" class="uk-icon-button uk-icon-facebook sosmed-ico"></span>
+                <span href="#" class="uk-icon-button uk-icon-twitter sosmed-ico"></span>
+                <span href="#" class="uk-icon-button uk-icon-instagram sosmed-ico"></span>
+                <span href="#" class="uk-icon-button uk-icon-google-plus sosmed-ico"></span>
+                 <span href="#" class="uk-icon-button uk-icon-pinterest sosmed-ico"></span>
+                  <span href="#" class="uk-icon-button uk-icon-linkedin sosmed-ico"></span>
+                </div>
+
+                <div class="uk-panel">
+                    <p>Copyright &copy; 2015 Powered By Serbadistro | Toko Online Distro Indonesia</p>
+                   
+                </div>
+
+            </div>
+        </div>
+
+
+<div id="searchcanvas" class="uk-offcanvas">
+    <div class="uk-offcanvas-bar">
+        <form class="uk-search">
+        <label></label>
+       <input class="uk-search-field" type="search" placeholder="Pencarian Produk">
+        </form>
+    </div>
+</div>
+
+
+ <div id="offcanvas" class="uk-offcanvas">
+            <div class="uk-offcanvas-bar">
+                <ul class="uk-nav uk-nav-offcanvas">
+                    <li class="uk-active">
+                        <a href="kategori.html">Kategori</a>
+                    </li>
+                    <li>
+                        <a href="layouts_portfolio.html">Portfolio</a>
+                    </li>
+                    <li>
+                        <a href="layouts_blog.html">Blog</a>
+                    </li>
+                    <li>
+                        <a href="layouts_documentation.html">Documentation</a>
+                    </li>
+                    <li>
+                        <a href="layouts_contact.html">Contact</a>
+                    </li>
+                    <li>
+                        <a href="layouts_login.html">Login</a>
+                    </li>
+                </ul>
+
+                <ul class="uk-nav uk-nav-offcanvas">
+                    <li class="uk-active">
+                        <a href="layouts_frontpage.html">Brand</a>
+                    </li>
+                    <li>
+                        <a href="layouts_portfolio.html">Portfolio</a>
+                    </li>
+                    <li>
+                        <a href="layouts_blog.html">Blog</a>
+                    </li>
+                    <li>
+                        <a href="layouts_documentation.html">Documentation</a>
+                    </li>
+                    <li>
+                        <a href="layouts_contact.html">Contact</a>
+                    </li>
+                    <li>
+                        <a href="layouts_login.html">Login</a>
+                    </li>
+                </ul>
+            </div>
+ </div>
+
+    </body>
+</html>
---
+++ kategori.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Web Design UI KIT</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+        <link rel="apple-touch-icon-precomposed" href="images/apple-touch-icon.png">
+         <link id="data-uikit-theme" rel="stylesheet" href="css/uikit.min.css">
+        <link id="data-uikit-theme" rel="stylesheet" href="css/uikit.docs.min.css">
+        <link rel="stylesheet" href="css/docs.css">
+        <script src="js/jquery.js"></script>
+        <script src="js/uikit.min.js"></script>
+        <script src="js/docs.js"></script>
+        <script src="js/components/slideshow.js"></script>
+        <script src="js/components/slideshow-fx.js"></script>
+         <script src="js/components/sticky.js"></script>
+         
+    </head>
+    <body>
+  
+  <div data-uk-sticky>
+<div class="uk-margin" style="margin-bottom:0px;">
+
+                                <nav class="uk-navbar">
+
+                                    <ul class="uk-navbar-nav">
+                                        <li ><a href="#offcanvas" class="uk-navbar-toggle " style="font-weight:bold" data-uk-offcanvas></a></li>
+                                       
+                                    </ul>
+
+                                    <div class="uk-navbar-flip">
+
+                                        <ul class="uk-navbar-nav">
+                                            <li>
+                                                <a href="keranjang.html" style="position:relative"><img src="images/cart.png"> <span style="color:#fff;position:absolute;top:0;right:23px;">0</span> </a>
+
+                                          
+
+                                            </li>
+                                           
+                                        </ul>
+
+                             </div>
+
+                                    <div class="uk-navbar-content uk-navbar-center" style="font-weight:bold">SERBADISTRO</div>
+
+                                </nav>
+
+                            </div>
+
+
+
+
+<div class="uk-grid uk-grid-collapse" style="background:#42505d;">
+<div class="uk-width-7-10" style="background:#42505d">
+    <ul class="uk-subnav uk-subnav-line  subnav-ideo" style="margin-left:10px;">
+                                <li><a href="#">News</a></li>
+                                <li><a href="#">Tshirt</a></li>
+                                <li><a href="#">Login</a></li>
+                                <li ><a href="#">Affaliate</a></li>
+                               
+                            </ul>
+
+</div>
+                                    <div class="uk-width-3-10">
+                                        
+<ul class="uk-subnav  subnav-ideo" style="float:right;margin-right:10px">
+ <li data-uk-dropdown="{mode:'click'}">
+                                    <a href="#">Akun <i class="uk-icon-caret-down"></i></a>
+                                    <div class="uk-dropdown uk-dropdown-small">
+                                        <ul class="uk-nav uk-nav-dropdown">
+                                            <li><a href="#">Item</a></li>
+                                            <li><a href="#">Another item</a></li>
+                                            <li class="uk-nav-header">Header</li>
+                                            <li><a href="#">Item</a></li>
+                                            <li><a href="#">Another item</a></li>
+                                            <li class="uk-nav-divider"></li>
+                                            <li><a href="#">Separated item</a></li>
+                                        </ul>
+                                    </div>
+                                </li>
+                                <li>
+                                 <a href="javascript:void(0)" data-uk-offcanvas="{target:'#searchcanvas'}"><i class="uk-icon-search"></i></a>
+
+                                </li>
+</ul>
+<div style="clear:both"></div>
+
+                                    </div>
+                                    
+                                </div>
+
+</div>
+
+ 
+                         
+
+<div class="uk-grid">
+    <div class="uk-width-1-1 " >
+   <ul class="uk-breadcrumb uk-margin-bottom uk-margin-top uk-margin-left">
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">Blog</a></li>
+                                <li><span>Category</span></li>
+                                <li class="uk-active"><span>Post</span></li>
+                            </ul>   
+    </div>
+
+  <div class="uk-width-1-1">
+  <div class="uk-margin uk-container-center uk-text-center" data-uk-margin="">
+                               
+                                <a class="uk-thumbnail uk-thumbnail-mini" href="detailproduk.html">
+                                    <img src="images/placeholder_200x100.svg" width="200" height="100" alt="">
+                                    <div class="uk-thumbnail-caption">Caption <code>&lt;a&gt;</code></div>
+                                </a>
+
+                                 <a class="uk-thumbnail uk-thumbnail-mini" href="#">
+                                    <img src="images/placeholder_200x100.svg" width="200" height="100" alt="">
+                                    <div class="uk-thumbnail-caption">Caption <code>&lt;a&gt;</code></div>
+                                </a>
+                                 <a class="uk-thumbnail uk-thumbnail-mini" href="#">
+                                    <img src="images/placeholder_200x100.svg" width="200" height="100" alt="">
+                                    <div class="uk-thumbnail-caption">Caption <code>&lt;a&gt;</code></div>
+                                </a>
+                                   <a class="uk-thumbnail uk-thumbnail-mini" href="#">
+                                    <img src="images/placeholder_200x100.svg" width="200" height="100" alt="">
+                                    <div class="uk-thumbnail-caption">Caption <code>&lt;a&gt;</code></div>
+                                </a>
+                                 </a>
+                                   <a class="uk-thumbnail uk-thumbnail-mini" href="#">
+                                    <img src="images/placeholder_200x100.svg" width="200" height="100" alt="">
+                                    <div class="uk-thumbnail-caption">Caption <code>&lt;a&gt;</code></div>
+                                </a>
+                                 </a>
+                                   <a class="uk-thumbnail uk-thumbnail-mini" href="#">
+                                    <img src="images/placeholder_200x100.svg" width="200" height="100" alt="">
+                                    <div class="uk-thumbnail-caption">Caption <code>&lt;a&gt;</code></div>
+                                </a>
+                            </div>
+
+                            </div>
+</div>
+
+
+<ul class="uk-pagination">
+                                <li class="uk-disabled"><span><i class="uk-icon-angle-double-left"></i></span></li>
+                                <li class="uk-active"><span>1</span></li>
+                                <li><a href="#">2</a></li>
+                                <li><a href="#">3</a></li>
+                                <li><a href="#">4</a></li>
+                                <li><span>...</span></li>
+                                <li><a href="#">20</a></li>
+                                <li><a href="#"><i class="uk-icon-angle-double-right"></i></a></li>
+                            </ul>
+
+
+
+
+<div class="tm-footer uk-margin-top" style="background:#42505d;">
+            <div class="uk-container uk-container-center uk-text-center">
+
+                <ul class="uk-subnav uk-subnav-line uk-flex-center">
+                    <li><a href="https://github.com/uikit/uikit">How To Order </a></li>
+                    <li><a href="https://github.com/uikit/uikit/issues">Return Excange</a></li>
+                    <li><a href="https://github.com/uikit/uikit/blob/master/CHANGELOG.md">Privacy And Security</a></li>
+                    <li><a href="https://twitter.com/getuikit">Shipping And Delivery</a></li>
+                </ul>
+
+                <div class="uk-margin-bottom sosmed-ico" style="color:#000">
+                <span href="#" class="uk-icon-button uk-icon-facebook sosmed-ico"></span>
+                <span href="#" class="uk-icon-button uk-icon-twitter sosmed-ico"></span>
+                <span href="#" class="uk-icon-button uk-icon-instagram sosmed-ico"></span>
+                <span href="#" class="uk-icon-button uk-icon-google-plus sosmed-ico"></span>
+                 <span href="#" class="uk-icon-button uk-icon-pinterest sosmed-ico"></span>
+                  <span href="#" class="uk-icon-button uk-icon-linkedin sosmed-ico"></span>
+                </div>
+
+                <div class="uk-panel">
+                    <p>Copyright &copy; 2015 Powered By Serbadistro | Toko Online Distro Indonesia</p>
+                   
+                </div>
+
+            </div>
+        </div>
+
+
+<div id="searchcanvas" class="uk-offcanvas">
+    <div class="uk-offcanvas-bar">
+        <form class="uk-search">
+        <label></label>
+       <input class="uk-search-field" type="search" placeholder="Pencarian Produk">
+        </form>
+    </div>
+</div>
+
+
+ <div id="offcanvas" class="uk-offcanvas">
+            <div class="uk-offcanvas-bar">
+                <ul class="uk-nav uk-nav-offcanvas">
+                    <li class="uk-active">
+                        <a href="layouts_frontpage.html">Kategori</a>
+                    </li>
+                    <li>
+                        <a href="layouts_portfolio.html">Portfolio</a>
+                    </li>
+                    <li>
+                        <a href="layouts_blog.html">Blog</a>
+                    </li>
+                    <li>
+                        <a href="layouts_documentation.html">Documentation</a>
+                    </li>
+                    <li>
+                        <a href="layouts_contact.html">Contact</a>
+                    </li>
+                    <li>
+                        <a href="layouts_login.html">Login</a>
+                    </li>
+                </ul>
+
+                <ul class="uk-nav uk-nav-offcanvas">
+                    <li class="uk-active">
+                        <a href="layouts_frontpage.html">Brand</a>
+                    </li>
+                    <li>
+                        <a href="layouts_portfolio.html">Portfolio</a>
+                    </li>
+                    <li>
+                        <a href="layouts_blog.html">Blog</a>
+                    </li>
+                    <li>
+                        <a href="layouts_documentation.html">Documentation</a>
+                    </li>
+                    <li>
+                        <a href="layouts_contact.html">Contact</a>
+                    </li>
+                    <li>
+                        <a href="layouts_login.html">Login</a>
+                    </li>
+                </ul>
+            </div>
+ </div>
+
+    </body>
+</html>
